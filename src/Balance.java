704d9ad2937e8387e2af6de0c9a268e240edf4bc
adam <adam@mvpsohio.com>
2024-06-04 16:40:06 -0400

    added a bankroll manager to keep users balances saved so they can login and logout but keep track of their balance, added user class

--- src/Balance.java
+++ src/Balance.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Balance {
+public class Balance {
+    private int amount;
+
+    public Balance(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void add(int amount) {
+        this.amount += amount;
+    }
+
+    public void deduct(int amount) {
+        if (this.amount >= amount) {
+            this.amount -= amount;
+        } else {
+            System.out.println("Insufficient balance.");
+        }
+    }
 }
